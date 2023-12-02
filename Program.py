2e1a0a35c59fadcd3d594f1f885f2d551a88da00
Niusha_ygn <niusha.yaghini@gmail.com>
2023-12-03 00:27:07 +0330

    getting answer with cpu limit = 100

--- Program.py
+++ Program.py
@@ -25,7 +25,7 @@ def Bee_Colony_Algorithm():
         result = open(f'{result_file_name}', 'a')    
         result.write(f"iteration number {iteration_num}: \n")
         currentTime = datetime.now().strftime("%H:%M:%S")
-        print(f"iteration number {iteration_num}: {currentTime}")
+        # print(f"iteration number {iteration_num}: {currentTime}")
         iteration_num+=1
  
         ABC = Artificial_Bee_Colony.ABC_algorithm(employed_bees_num, nK, nI, Capacity, Profits, Weights, onlooker_bees_num, max_improvement_try, pc, pm, k_tournomet_percent, percedure_type)
@@ -41,8 +41,8 @@ def Bee_Colony_Algorithm():
         result.write(f"best bee => data: {best_bee_of_iteration.data}, fitness: {best_fitness_of_iteration}\n")  
         result.write(f"best fitness so far: {best_fitness_so_far}\n")
 
-        print(f"best fitness of iteration = {best_fitness_of_iteration}")
-        print(f"best fitness so far: {best_fitness_so_far}")
+        # print(f"best fitness of iteration = {best_fitness_of_iteration}")
+        # print(f"best fitness so far: {best_fitness_so_far}")
 
         ABC.scout_bees(population)
         
@@ -72,14 +72,14 @@ if __name__ == '__main__':
     percedure_type = "Roulette Wheel"
     cross_over_type = "one_point"
     
-    cpu_time_limit = 1; # second
+    cpu_time_limit = 100; # second
     
     # file name of the datas
-    data_file_name = ".\\mknap1-Question\\01.txt"
+    data_file_name = ".\\mknap1-Question\\07.txt"
     
     # file name for save results
-    result_file_name = ".\\mknap1-Answer(try1)\\01.txt"
-    photo_name = "01"
+    result_file_name = ".\\mknap1-Answer(try1)\\07.txt"
+    photo_name = "07"
 
     # nK = number of knapstacks
     # nI = number of items
